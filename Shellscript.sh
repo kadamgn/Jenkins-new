5f16f1c813dac55b4dbe5623a128f77eb139c3cf
kadamgn <91948742+kadamgn@users.noreply.github.com>
2022-02-24 14:35:34 +0530

    Update Shellscript.sh

--- Shellscript.sh
+++ Shellscript.sh
@@ -3,3 +3,6 @@ echo "this is a jenkins job"
 echo $USER
 echo $PATH
 echo $BUILD_NUMBER
+echo $HOSTNAME
+echo "user as selected region as $REGION_CHOICE"
+echo "user as selected instance_type as $instance_type"
